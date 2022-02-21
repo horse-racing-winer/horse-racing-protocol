e98bf3dbda35551a47cb468ead7ac45fefe7a8a4
Zhichao Zhang <zzcwoshizz@163.com>
2022-02-21 13:32:33 +0800

    game

--- packages/contracts/scripts/run-game.ts
+++ packages/contracts/scripts/run-game.ts
@@ -7,30 +7,6 @@ import { getContract } from '../utils/deploy';
 async function main() {
   const { owner } = await getNamedAccounts();
   const game = await getContract('Game', owner);
-  const wallet = new Wallet('0xa74d076d53232d4a7743502f47d667a330a807c54689af2cc55511339aec2de0');
-
-  const signature = await wallet.signMessage(
-    keccak256(
-      ethers.utils.defaultAbiCoder.encode(
-        ['address', 'uint256'],
-        ['0x8D5BE0CF7a4FA0680AFF798516c259AC0eE57334', parseEther('200')]
-      )
-    ).slice(2)
-  );
-
-  console.log(signature);
-
-  console.log(
-    verifyMessage(
-      keccak256(
-        ethers.utils.defaultAbiCoder.encode(
-          ['address', 'uint256'],
-          ['0x8D5BE0CF7a4FA0680AFF798516c259AC0eE57334', parseEther('200')]
-        )
-      ).slice(2),
-      signature
-    )
-  );
 }
 
 main().catch(console.error);
